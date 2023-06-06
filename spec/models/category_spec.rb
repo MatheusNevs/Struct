6ebdbcd958214336d6336ef48f4d82a4e5a0367d
MatheusNevs <matheusnf62@gmail.com>
2023-06-06 20:55:00 -0300

    testing category model without validates

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  context "validates - " do
+    it "it should be valid" do 
+      expect(build(:category)).to be_valid
+    end
+
+    it "nil title should'nt be valid" do
+      expect(build(:category, title:nil)).to be_invalid
+    end
+
+    it "nill description shouldn't be valid" do
+      expect(build(:category, description:nil)).to be_invalid
+    end
+  end
+end

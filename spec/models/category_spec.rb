b505143a410dcf30dae64fc8196999b56ed0ae11
MatheusNevs <matheusnf62@gmail.com>
2023-06-06 21:09:42 -0300

    testing category model with validates defined

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe Category, type: :model do
       expect(build(:category)).to be_valid
     end
 
+    it "category with new name should be valid" do
+      create(:category, title: 'Fofoca')
+      expect(build(:category)).to be_valid
+    end
+
     it "nil title should'nt be valid" do
       expect(build(:category, title:nil)).to be_invalid
     end
 
     it "nill description shouldn't be valid" do
       expect(build(:category, description:nil)).to be_invalid
+    end
+
+    it "category title already created should be invalid" do
+      create(:category, title:'Fofoca')
+      expect(build(:category, title: 'Fofoca')).to be_invalid
     end
   end
 end

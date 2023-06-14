b55b59aa432681fc76b6dfcfd1898b002751c20c
MatheusNevs <matheusnf62@gmail.com>
2023-06-13 21:35:09 -0300

    refs defined

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     validates :title, presence: :true, uniqueness: :true
     validates :description, presence: :true
+
+    has_many :group_categories
 end
--- app/models/group_category.rb
+++ app/models/group_category.rb
@@ -1,4 +1,6 @@
 class GroupCategory < ApplicationRecord
     validates :category_id, presence: :true
     validates :group_id, presence: :true
+    belongs_to :group
+    belongs_to :category
 end

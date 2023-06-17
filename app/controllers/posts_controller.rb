9d22295919727c0292977222f0fb70abdce73658
MatheusNevs <matheusnf62@gmail.com>
2023-06-17 17:38:40 -0300

    posts_controller' CRUD defined

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,47 @@
 class PostsController < ApplicationController
+    def index
+        render json: Post.all, status: :ok
+    rescue StandardError => e
+        render json: {error:e.message}, status: :bad_request
+    end
     
+    def show
+        render json: Post.find(params[:id]), status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
+    end
+    
+    def create
+        post = Post.new(post_params)
+        post.save!
+        render json: post, status: :created
+    rescue StandardError => e
+        render json: { error: e.message }, status: :bad_request
+    end
+    
+    def update
+        render json: Post.update!(params[:id], post_params), status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
+    rescue StandardError => e
+        render json: { error: e.message }, status: :bad_request
+    end
+    
+    def delete
+        post = Post.find(params[:id])
+        post.destroy!
+        render json: post, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
+    rescue StandardError => e
+        render json: { error: e.message }, status: :bad_request
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :content, :id
+            
+        )
+    end
 end

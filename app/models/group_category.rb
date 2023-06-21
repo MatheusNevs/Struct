3d4c5f24c2073caf0aa90b198e04ba7e395e20a4
MatheusNevs <matheusnf62@gmail.com>
2023-06-21 11:10:47 -0300

    adjusting models

--- app/models/group_category.rb
+++ app/models/group_category.rb
@@ -1,7 +1,4 @@
-class GroupCategory < ApplicationRecord
-    validates :category_id, presence: :true
-    validates :group_id, presence: :true
-    
+class GroupCategory < ApplicationRecord 
     belongs_to :group
     belongs_to :category
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,6 @@ class Post < ApplicationRecord
 
     has_many :post_categories
     has_many :categories, through: :post_category
-<<<<<<< HEAD
     has_many :feedbacks
-=======
->>>>>>> 54c3abcf113705bae24aed88f461def3b9031411
     has_many :comments
 end
